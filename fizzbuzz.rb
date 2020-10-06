e0e56ffaf5044581257ab550d0dea03d3d070ba9
Alex Marz <alexhmarz@gmail.com>
2020-10-06 18:59:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 
-    puts "Fizz"
+    "Fizz"
     elsif int % 5 == 0 
-    puts "Buzz"
+    "Buzz"
     elsif int % 3 == 0 && int % 5 == 0 
-    puts "FizzBuzz"
+    "FizzBuzz"
   end
 end
